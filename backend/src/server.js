1e0f51c0591e3fdcbb0ef99905d03781d3e45bea
UnstaBleSos <Rambahadru@gmail.com>
2026-08-13 14:29:11 +0545

    Initialize backend with express.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
+require("dotenv").config()
+
 const express = require("express")
 
+const testRoutes = require("./routes/testRoute")
+
 const app = express()
+
+app.use("/api/test",testRoutes)
 
 app.get("/",(req,res)=>{
     res.json({message:"Backend is running"})
 })
 
-
-const PORT = 5000
+const PORT = process.env.PORT
 
 app.listen(PORT,()=>{
-    console.log(`Server should be running on https://localhost:${PORT}`)
+    console.log(`Server should be running on http://localhost:${PORT}`)
 })
